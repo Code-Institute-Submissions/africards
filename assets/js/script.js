403c0f4b846b33a9a903c3c148831aa2e627203f
Jess Erhabor <jess.erhabor@gmail.com>
2020-10-04 15:11:40 +0100

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Game Variables
+
+const southAfrica = ["biltong", "bunnychow", "elephant", "gemsbok", "giraffe", "lion", "meerkat", "pap"];
+
+
+var timeLeft = 60;
+var flipCount = 0;
+
+
+$(document).ready(function() {
+
+
+
+/** Function for the Light-Dark Theme Toggle **/
+  $("#theme-toggle").click(function() {
+    var lightDarkSwitch = $("#stylesheet");
+    if (lightDarkSwitch.attr("href") == "assets/css/style.css") {
+      lightDarkSwitch.attr("href", "assets/css/style-dark.css");
+      console.log($("#stylesheet").attr("href"));
+    }  else {
+      lightDarkSwitch.attr("href", "assets/css/style.css");
+      console.log($("#stylesheet").attr("href"));
+    }
+  });
+})
