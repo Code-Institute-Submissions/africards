552bada5ef639717ae41fae47f001d7634f00081
Jess Erhabor <jess.erhabor@gmail.com>
2020-10-07 13:14:02 +0100

    FEAT: Created the flip and match mechanism. If unmatched, the user will have half a second to see the cards before they flip over to the back.

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,27 +24,41 @@ $(document).ready(function() {
             $('.card').each(function(index) {
                 $(this).attr('data-card-value', cards[index]);
             });
-            game.clickHandlers();
+            game.clickHandler();
         },
 
-        clickHandlers: function() {
-            $('.card').click(function() {
+        clickHandler: function() {
+            if ($(".card").hasClass("unmatched")) {
+                $('.unmatched').click(function() {
+                    $(this).addClass("visible");
+                    console.log($(this).data('cardValue'));
+                    if (game.firstCard.length === 0) {
+                        game.firstCard = [];
+                        game.firstCard.push($(this).data('cardValue'));
+                        $(this).addClass("checkForMatch");
+                    } else if (game.firstCard.length >= 1 && game.secondCard.length === 0){
+                        game.secondCard = [];
+                        game.secondCard.push($(this).data('cardValue'));
+                        $(this).addClass("checkForMatch");
+                        game.checkMatch();
+                    };
+                    console.log(game.firstCard, game.secondCard);
+                });
+            };
+        },
 
-                $(this).addClass("visible");
-                console.log($(this).data('cardValue'));
-
-
-                if (game.firstCard.length >= 0) {
-                    game.firstCard = [];
-                    game.firstCard.push($(this).data('cardValue'));
-                } else {
-                    game.secondCard = [];
-                    game.secondCard.push($(this).data('cardValue'));
-                };
-
-                console.log(game.firstCard, game.secondCard);
-            });
-            // game.checkMatch();
+        checkMatch: function() {
+            if (game.firstCard[0] === game.secondCard[0]) {
+                $(".visible").addClass("matched").removeClass("unmatched").removeClass("checkForMatch");
+                game.firstCard = [];
+                game.secondCard = [];
+            } else {
+                setTimeout(function() {
+                    $(".unmatched").removeClass("visible").removeClass("checkForMatch");
+                }, 500)
+                game.firstCard = [];
+                game.secondCard = [];
+            };
         }
     };
 
