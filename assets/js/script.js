59e660235a3c5ac0e9afb9f581fe9349e52c6955
Jess Erhabor <jess.erhabor@gmail.com>
2020-10-15 14:48:43 +0100

    MAINT: Adding comments to the code to make it easier to understand for both myself and other developers.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Game Variables
+/* ----------------------------------------------------------------- List of Global Variables */
 
 var cards = [
     {
@@ -85,24 +85,17 @@ var movesCount = 0;
 var userCountry = localStorage.getItem("userCountry");
 var userName = localStorage.getItem("userName");
 
+/* ----------------------------------------------------------------- List of Game Play functions */
 
 $(document).ready(function() {
 
-    /* The following code allows user information from the modal to be displayed on the page for a more personalised experience.
-    The user is also required to select a country to visit which will tailor the game cards to the country.*/
+    /* ------------------------------------------------------------- Checking User Data */
 
-
-
-
-
-
-
-
-
+    /* The following code allows user information from the modal
+    to be displayed on the page for a more personalised experience.*/
 
     function checkUserData() {
         if ((userCountry === null) || (userCountry === "") || (userName === "")) {
-            // Taken from https://stackoverflow.com/questions/22207377/disable-click-outside-of-bootstrap-modal-area-to-close-modal
             $("#userProfileModal").modal({
                 backdrop: 'static',
                 keyboard: false
@@ -111,12 +104,18 @@ $(document).ready(function() {
             userName = localStorage.getItem("userName");
             userCountry = localStorage.getItem("userCountry");
         }
-        setupUser(userCountry, userName);
+        setUpUser(userCountry, userName);
     }
     checkUserData();
 
+    /* ------------------------------------------------------------- Setting Up User Profile */
 
-    function setupUser(userCountry, userName) {
+    /*The user is required to select a country to visit which
+    will tailor the game cards to the country. For example, if user
+    selects "Nigeria", the game cards will have the Nigerian flag on
+    the back face and cultural images on the front face.*/
+
+    function setUpUser(userCountry, userName) {
         $('#user-name').text(userName);
         $('#user-country').text("Welcome" + " To " + userCountry + "!");
 
@@ -142,7 +141,15 @@ $(document).ready(function() {
         shuffleDeck();
     }
 
-    /* Shuffle method */
+    /* ------------------------------------------------------------- Shuffling of Card Deck */
+
+    /* This shuffle method creates a loop for numbers starting
+    from the first index i.e. i=0. The maximum value of i will
+    be 15 (which will be the last index of the cards array). It
+    creates a random value between 0 and 15 inclusive for each
+    index in the cards array. It then rounds them and reassigns them back
+    to the array; the result is that the indices have now changed values.*/
+
     function shuffleDeck() {
         var random = 0;
         var temp = 0;
@@ -156,67 +163,13 @@ $(document).ready(function() {
         assignDeck();
     }
 
+    /* ------------------------------------------------------------- Assigning Card Deck */
 
-    function loadTimer() {
-        $('#easy-level').click(function() {
-            difficulty = 'easy';
-            secondsLeftDisplay.text(easyTimer);
-            defaultLevelTime = secondsLeftDisplay.text();
-        });
-        $('#medium-level').click(function() {
-            difficulty = 'medium';
-            secondsLeftDisplay.text(mediumTimer);
-            defaultLevelTime = secondsLeftDisplay.text();
-        });
-        $('#hard-level').click(function() {
-            difficulty = 'hard';
-            secondsLeftDisplay.text(hardTimer);
-            defaultLevelTime = secondsLeftDisplay.text();
-        });
-    };
-
-    function hardCountDownTimer() {
-        deactivatedMode()
-        var timer = setInterval(function() {
-            if (hardTimer <= 1) {
-                clearInterval(timer);
-                $("#game-over-text").addClass("visible");
-                resetGame();
-            } else {
-                hardTimer--;
-                secondsLeftDisplay.text(hardTimer);
-            };
-        }, 1000);
-        secondsLeftDisplay.text(hardTimer);
-    }
-    function mediumCountDownTimer() {
-        deactivatedMode()
-        var timer = setInterval(function() {
-            if (mediumTimer <= 1) {
-                clearInterval(timer);
-                $("#game-over-text").addClass("visible");
-                resetGame();
-            } else {
-                mediumTimer--;
-                secondsLeftDisplay.text(mediumTimer);
-            };
-        }, 1000);
-        secondsLeftDisplay.text(mediumTimer);
-    }
-    function easyCountDownTimer() {
-        deactivatedMode()
-        var timer = setInterval(function() {
-            if (easyTimer <= 1) {
-                clearInterval(timer);
-                $("#game-over-text").addClass("visible");
-                resetGame();
-            } else {
-                easyTimer--;
-                secondsLeftDisplay.text(easyTimer);
-            };
-        }, 1000);
-        secondsLeftDisplay.text(easyTimer);
-    }
+    /* This function assigns the shuffled card images to their
+    corresponding new indices. For the matching mechanism, a
+    'data-card-value' attribute has been created. This will match
+    the image name. For example, where the image is 'one.jpg', the
+    'data-card-value' of this card would be 1.*/
 
     function assignDeck() {
         $('.card').each(function(index) {
@@ -229,28 +182,23 @@ $(document).ready(function() {
         clickHandlers();
     }
 
+    /* ------------------------------------------------------------- Click Handlers */
+
     function clickHandlers() {
-        // $(".overlay-text-small").click(function() {
-        //     $("#game-over-text").removeClass("visible");
-        // });
-
         $('#save-button').on('click', function() {
-
-            // Consider using 'Switch Case' instead?
             let userName = $('#userName').val();
             let userCountry = $('#inputCountry').val();
 
             localStorage.setItem("userCountry", userCountry);
             localStorage.setItem("userName", userName);
-            setupUser(userCountry, userName);
+            setUpUser(userCountry, userName);
 
             $('#userProfileModal').modal('hide');
         });
 
-        $('body').delegate('.unmatched', "click", function() {
+        $('body').delegate('.unmatched', "click", function() { // This line allows only the cards with the unmatched class to be selected. If matched, the function does not apply.
             $(this).addClass("visible");
             if (firstCard.length === 0) {
-                firstCard = [];
                 firstCard.push($(this).data('cardValue'));
                 $(this).addClass("checkForMatch").removeClass('unmatched');
             } else if (firstCard.length >= 1 && secondCard.length === 0){
@@ -275,12 +223,12 @@ $(document).ready(function() {
 
     function checkMatch() {
         if (firstCard[0] === secondCard[0]) {
-            $(".visible").addClass("matched").removeClass("unmatched").removeClass("checkForMatch");
+            $(".visible").addClass("matched").removeClass("unmatched checkForMatch");
             firstCard = [];
             secondCard = [];
         } else {
             setTimeout(function() {
-                $(".checkForMatch").removeClass("visible").removeClass("checkForMatch").addClass('unmatched');
+                $(".checkForMatch").removeClass("visible checkForMatch").addClass('unmatched');
                 firstCard = [];
                 secondCard = [];
             }, 500)
@@ -300,24 +248,103 @@ $(document).ready(function() {
 
     function resetGame() {
         $(".overlay-text-small").click(function() {
-          // timer;
+            // timer;
             $('.card').removeClass('visible matched checkForMatch').addClass('unmatched');
-            $("button").prop("disabled", false).removeClass('deactivatedMode');
+            $(".level-container button").prop("disabled", false).removeClass('deactivatedMode');
             // return false;
+            loadTimer();
             console.log(defaultLevelTime);
             secondsLeftDisplay.text(defaultLevelTime);
-            $('#moves').text(0)
+            difficulty;
+            shuffleDeck();
+            movesCount = 0;
+            $('#moves').text(movesCount);
             $('.overlay-text').removeClass('visible');
             firstCard = [];
             secondCard = [];
+            $('.level-selector overlay-text').addClass('visible');
         });
+    }
+
+    function loadTimer() {
+        $('#easy-level').click(function() {
+            difficulty = 'easy';
+            let defaultLevelTime = secondsLeftDisplay.text(easyTimer);
+            defaultLevelTime;
+        });
+        $('#medium-level').click(function() {
+            difficulty = 'medium';
+            let defaultLevelTime = secondsLeftDisplay.text(mediumTimer);
+            defaultLevelTime;
+        });
+        $('#hard-level').click(function() {
+            difficulty = 'hard';
+            let defaultLevelTime = secondsLeftDisplay.text(hardTimer);
+            defaultLevelTime;
+        });
+    };
+
+    function hardCountDownTimer() {
+        deactivatedMode()
+        var timer = setInterval(function() {
+            if (hardTimer <= 1) {
+                clearInterval(timer);
+                $("#game-over-text").addClass("visible");
+            } else {
+                hardTimer--;
+                secondsLeftDisplay.text(hardTimer);
+            };
+        }, 1000);
+        secondsLeftDisplay.text(hardTimer);
+    }
+
+    function mediumCountDownTimer() {
+        deactivatedMode()
+        var timer = setInterval(function() {
+            if (mediumTimer <= 1) {
+                clearInterval(timer);
+                $("#game-over-text").addClass("visible");
+            } else {
+                mediumTimer--;
+                secondsLeftDisplay.text(mediumTimer);
+            };
+        }, 1000);
+        secondsLeftDisplay.text(mediumTimer);
+    }
+
+    function easyCountDownTimer() {
+        deactivatedMode()
+        var timer = setInterval(function() {
+            if (easyTimer <= 1) {
+                clearInterval(timer);
+                $("#game-over-text").addClass("visible");
+            } else {
+                easyTimer--;
+                secondsLeftDisplay.text(easyTimer);
+            };
+        }, 1000);
+        secondsLeftDisplay.text(easyTimer);
     }
 
     function deactivatedMode() {
         //Disables buttons
-        $("button").prop("disabled", true).addClass('deactivatedMode');
+        $(".level-container button").prop("disabled", true).addClass('deactivatedMode');
         return true;
     }
+
+    // function newCountdown() {
+    //     var timer = setInterval(function() {
+    //         if ($('#seconds') <= 1) {
+    //             clearInterval(timer);
+    //             $("#game-over-text").addClass("visible");
+    //             resetGame();
+    //         } else {
+    //             $('#seconds')--;
+    //             secondsLeftDisplay.text($('#seconds'));
+    //         };
+    //     }, 1000);
+    //     secondsLeftDisplay.text($('#seconds'));
+    // }
 
 
     /* Function for the Light-Dark Theme Toggle */
